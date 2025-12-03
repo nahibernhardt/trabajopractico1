008868cdd5b1eaddde334c54de655bde91a4e8b9
nahibernhardt <nahibernhardt77@gmail.com>
2025-12-03 02:16:40 -0300

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,15 @@
+function oscuro() {document.body.style.backgroundColor= "black";
+document.getElementById("humitas").style.color= "white";
+document.getElementById("malo").style.color= "white";
+document.getElementById("papoi").style.color= "white";
+document.getElementById("lolis").style.color= "white";
+document.getElementById("lolis").style.color= "white";
+document.querySelectorAll('li').forEach(li => li.style.color = 'white');
+document.getElementById("nenu").style.color= "white";
+document.getElementById("katy").style.color= "white";
+document.getElementById("oli").style.color= "white";
+document.getElementById("hoji").style.color= "white";
+document.getElementById("pichu").style.color= "white";
+}
+
+function info() {alert ("Nahiara Bernhardt. Programadora Web. nahibernhardt77@gmail.com")}
